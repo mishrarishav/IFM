679f29427472f15ea260e43f8e4be75d6d115d52
mishrarishav <101666861+mishrarishav@users.noreply.github.com>
2024-10-08 13:47:18 +0530

    not

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const sql = require('mssql');
 const { ipcMain } = require('electron');
 async function initializeDatabase() {
-     const connectionString = "Data Source=localhost,1433;Initial Catalog=scanning_validation_tnco;User Id=rishav;Password=asdf@123;TrustServerCertificate=true;";
-   //const connectionString = "Data Source=BAWINMS001,1433;Initial Catalog=scanning_validation_tnco;User Id=sa;Password=Baw_sql22;TrustServerCertificate=true;";
+      const connectionString = "Data Source=localhost,1433;Initial Catalog=scanning_validation_tnco;User Id=rishav;Password=asdf@123;TrustServerCertificate=true;";
+  //  const connectionString = "Data Source=BAWINMS001,1433;Initial Catalog=scanning_validation_tnco;User Id=sa;Password=Baw_sql22;TrustServerCertificate=true;";
   
   try {
     const pool = await sql.connect(connectionString);
